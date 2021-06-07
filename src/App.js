6fc56c7d6a38f11e0dcbdf1e8d9e525721bc4642
RoaaMustafa <labushanab14@gmail.com>
2021-06-07 15:19:11 +0300

    coding

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Main from './components/Main';
+import Header from './componemt/Header';
+import Footer from './componemt/Footer';
+import Main from './componemt/Main';
 
-class App extends React.Component {
-  render() {
-    return (
+class App extends React.Component{
+
+  render(){
+    return(
       <div>
-        <Header />
-        <Main />
-        <Footer />
+       <Header />
+       <Main />
+       <Footer />
       </div>
-    )
+    );
   }
 }
 
